adad48e6d9519ab701bf933d65457a109a394e97
jodersi <anderjerry1@gmail.com>
2025-02-25 05:17:43 -0600

    login and registration page basic functionality

--- app/src/main/java/com/example/aidruginteractionchecker/LoginPage.kt
+++ app/src/main/java/com/example/aidruginteractionchecker/LoginPage.kt
@@ -1,23 +1,72 @@
 package com.example.aidruginteractionchecker
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.EditText
+import android.widget.ProgressBar
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginPage : AppCompatActivity() {
+
+    lateinit var firebaseAuth: FirebaseAuth
+
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login_page)
 
-        val btnLogin = findViewById<Button>(R.id.loginBtn) //temporary code to send to main on click
-        btnLogin.setOnClickListener(){
+/*        val btnLogin = findViewById<Button>(R.id.loginBtn) //temporary code to send to main on click
+        btnLogin.setOnClickListener{
             val mainPageSkip = Intent(this, MainActivity::class.java)
             startActivity(mainPageSkip)
+        }*/
+
+        firebaseAuth = FirebaseAuth.getInstance()
+        val progressBar = findViewById<ProgressBar>(R.id.progressBarLogin) //sets progress bar to variable
+        val btnLogin = findViewById<Button>(R.id.loginBtn)
+        btnLogin.setOnClickListener{ //when login button clicked
+            progressBar.visibility = View.VISIBLE
+            val email = findViewById<EditText>(R.id.loginEmail).text.toString()
+            val password = findViewById<EditText>(R.id.loginPassword).text.toString()
+
+            if (email.isNotEmpty() && password.isNotEmpty()) { //if all fields are filled
+                firebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener { //logs into account
+                        if (it.isSuccessful) { //if successful
+                            progressBar.visibility = View.INVISIBLE
+                            val mainPageSkip = Intent(this, MainActivity::class.java)
+                            startActivity(mainPageSkip) //goes to main page
+                        } else { //if unsuccessful
+                            Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show() //gives error
+                            progressBar.visibility = View.INVISIBLE
+                        }
+                    }
+            }
+            else {
+                progressBar.visibility = View.INVISIBLE
+                Toast.makeText(this, "Fill all empty fields!", Toast.LENGTH_SHORT).show()//gives error
+            }
         }
+
+
+
+
+
+        //Go to Registration Page Functionality
+        val goToRegister = findViewById<TextView>(R.id.textGoToRegister)
+        goToRegister.setOnClickListener{
+            val sendToRegister = Intent(this, RegistrationPage::class.java)
+            startActivity(sendToRegister)
+        }
+
 
     }
 }
--- app/src/main/java/com/example/aidruginteractionchecker/RegistrationPage.kt
+++ app/src/main/java/com/example/aidruginteractionchecker/RegistrationPage.kt
@@ -1,20 +1,76 @@
 package com.example.aidruginteractionchecker
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ProgressBar
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.auth.FirebaseAuth
 
 class RegistrationPage : AppCompatActivity() {
+
+    lateinit var firebaseAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_registration_page)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        // Register Button Functionality
+        val progressBar = findViewById<ProgressBar>(R.id.progressBar) //sets progress bar to variable
+        val btnRegister = findViewById<Button>(R.id.registerBtn) //sets register button to variable
+        btnRegister.setOnClickListener{ //when register button clicked
+            progressBar.visibility = View.VISIBLE
+            val email = findViewById<EditText>(R.id.registerEmail).text.toString()
+            val password = findViewById<EditText>(R.id.registerPassword).text.toString()
+            val retypePassword = findViewById<EditText>(R.id.registerRetypePassword).text.toString()
+//            println(email)
+//            println(password)
+//            println(retypePassword)
+
+            if (email.isNotEmpty() && password.isNotEmpty() && retypePassword.isNotEmpty()) { //if all fields are filled
+                if (password == retypePassword) { //if password and retyped password are the same
+                    if (password.length >= 6){ //if password is at least 6 characters long
+                        firebaseAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener{ //makes account
+                            if (it.isSuccessful) { //if successful
+                                progressBar.visibility = View.INVISIBLE //progress bar invisible
+                                Toast.makeText(this, "Account successfully created, go to login page and sign in!", Toast.LENGTH_LONG).show() //gives message that account made
+                            }else { //if unsuccessful
+                                progressBar.visibility = View.INVISIBLE //progress bar invisible
+                                Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show() //gives error
+                            }
+                        }
+                    }
+                    else{
+                        Toast.makeText(this, "Password must at least 6 characters!", Toast.LENGTH_SHORT).show() //gives error
+                        progressBar.visibility = View.INVISIBLE //progress bar invisible
+                    }
+                }
+                else{
+                    Toast.makeText(this, "Password is not matching!", Toast.LENGTH_SHORT).show()//gives error
+                    progressBar.visibility = View.INVISIBLE //progress bar invisible
+                }
+            }
+            else {
+                Toast.makeText(this, "Fill all empty fields!", Toast.LENGTH_SHORT).show()//gives error
+                progressBar.visibility = View.INVISIBLE //progress bar invisible
+            }
+        }
+
+        //Switch To Login Functionality
+        val goToLogin = findViewById<TextView>(R.id.textGoToLogin)
+        goToLogin.setOnClickListener{
+            val sendToLogin = Intent(this, LoginPage::class.java)
+            startActivity(sendToLogin)
         }
     }
+
 }
